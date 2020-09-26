e197a113f773ed1d506d04d85596417b7a3fda4e
janjagusch <jan.jagusch@gmail.com>
2020-09-26 15:40:18 +0200

    remove object creation from outside main

--- dht_to_gbq/__init__.py
+++ dht_to_gbq/__init__.py
@@ -9,10 +9,8 @@ from datetime import datetime
 import Adafruit_DHT
 from google.cloud import bigquery
 
-_SENSOR = Adafruit_DHT.DHT22
 
-
-def _dht_data(sensor_id, sensor_pin):
+def _dht_data(sensor, sensor_id, sensor_pin):
     """
     Returns a tuple (humidity, temperature) from sensor.
     """
@@ -20,25 +18,29 @@ def _dht_data(sensor_id, sensor_pin):
     dht_data["sensor_id"] = sensor_id
     dht_data["requested_at"] = datetime.now()
     dht_data["humidity"], dht_data["temperature"] = Adafruit_DHT.read_retry(
-        _SENSOR, sensor_pin
+        sensor, sensor_pin
     )
     return dht_data
 
 
-def _gbq_insert(dht_data, project_id, dataset_id, table_id):
+def _gbq_setup(project_id, dataset_id, table_id):
+    """
+    Creates a BigQuery client.
+    """
+    client = bigquery.Client()
+    dataset = bigquery.dataset.DatasetReference.from_string(
+        f"{project_id}.{dataset_id}"
+    )
+    table = client.get_table(dataset.table(table_id))
+    return client, table
+
+
+def _gbq_insert(dht_data, client, table):
     """
     Inserts dht data into BigQuery.
     """
-    client = bigquery.Client()
-
-    def _table(project_id, dataset_id, table_id):
-        dataset = bigquery.dataset.DatasetReference.from_string(
-            f"{project_id}.{dataset_id}"
-        )
-        return client.get_table(dataset.table(table_id))
-
     dht_data["inserted_at"] = datetime.now()
-    errors = client.insert_rows(_table(project_id, dataset_id, table_id), [dht_data])
+    errors = client.insert_rows(table, [dht_data])
     if errors:
         raise RuntimeError(errors)
 
@@ -51,11 +53,13 @@ def main():
     sensor_pin = int(os.environ["DHT_SENSOR_PIN"])
     gbq_insert = bool(os.environ.get("DHT_GBQ_INSERT", False))
 
-    dht_data = _dht_data(sensor_id, sensor_pin)
+    sensor = Adafruit_DHT.DHT22
+    dht_data = _dht_data(sensor, sensor_id, sensor_pin)
     print(dht_data)
 
     if gbq_insert:
+        print("Inserting into BigQuery.")
         project_id = os.environ["DHT_GBQ_PROJECT_ID"]
         dataset_id = os.environ["DHT_GBQ_DATASET_ID"]
         table_id = os.environ["DHT_GBQ_TABLE_ID"]
-        _gbq_insert(dht_data, project_id, dataset_id, table_id)
+        _gbq_insert(dht_data, *_gbq_setup(project_id, dataset_id, table_id))
